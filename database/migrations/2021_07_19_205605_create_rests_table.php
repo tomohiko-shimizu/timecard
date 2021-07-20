492312508405fc2c0e01f8956287a9238623e45b
mio <omisiya@gmail.com>
2021-07-20 22:20:52 +0900

    retry

---
+++ database/migrations/2021_07_19_205605_create_rests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rests', function (Blueprint $table) {
+            $table->id();
+            $table->integer('timecard_id');
+            $table->timestamp('rest_start');
+            $table->timestamp('rest_finish')->nullable();
+            $table->timestamps();
+
+            // $table->foreign('timecard_id')->references('id')->on('timecards');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rests');
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->count(3)->create();
+        // factory(App\User::class)->create();
+    }
+}
