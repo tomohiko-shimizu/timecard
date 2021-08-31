76e450c20c6c161a149383fa680cabf84f8e6fc0
冷水友彦 <tomohiko@reisuitomohiko-no-MacBook-Pro.local>
2021-08-31 20:45:00 +0900

    メソッドの切り出し

--- app/Models/Timecard.php
+++ app/Models/Timecard.php
@@ -30,41 +30,44 @@ class Timecard extends Model
     //     $today = Carbon::today();
     //     return Timecard::where('user_id', $user->id)->where('date', $today);
     // }
-
-    public function getRestTime()
+    private function diffTime($start, $finish)
+    {
+        $c_start = new Carbon($start);
+        $c_finish = new Carbon($finish);
+        $time = $c_finish->diffInSeconds($c_start);
+        return $time;
+    }
+    private function getSumRestTime()
     {
         $rests = $this->rests;
         $sum_rest_time = 0;
-        foreach ($rests as $rest) {
-            $rest_start = new Carbon($rest->rest_start);
-            $rest_finish = new Carbon($rest->rest_finish);
-            $rest_time = $rest_finish->diffInSeconds($rest_start);
+        foreach ($rests as $rest) {   
+            $rest_time = $this->diffTime($rest->rest_start, $rest->rest_finish);
             $sum_rest_time = $sum_rest_time + $rest_time;
         }
-        $rest_time_hour = sprintf('%02d', floor($sum_rest_time / 3600));
-        $rest_time_minute = sprintf('%02d', floor(($sum_rest_time / 60) % 60));
-        $rest_time_second = sprintf('%02d', $sum_rest_time % 60);
-        return "${rest_time_hour}:${rest_time_minute}:${rest_time_second}";
+        return $sum_rest_time;
+    }
+
+    public function getRestTime()
+    {
+        $sum_rest_time = $this->getSumRestTime();
+        $this->formatTime($sum_rest_time);
+        return $this->formatTime($sum_rest_time);
+    }
+
+    private function formatTime($time)
+    {
+        $time_hour = sprintf('%02d', floor($time / 3600));
+        $time_minute = sprintf('%02d', floor(($time / 60) % 60));
+        $time_second = sprintf('%02d', $time % 60);
+        return "${time_hour}:${time_minute}:${time_second}";
     }
 
     public function getWorkTime()
     {
-        $work_start = new Carbon($this->work_start);
-        $work_finish = new Carbon($this->work_finish);
-        $work_time = $work_finish->diffInSeconds($work_start);
-
-        $rests = $this->rests;
-        $sum_rest_time = 0;
-        foreach ($rests as $rest) {
-            $rest_start = new Carbon($rest->rest_start);
-            $rest_finish = new Carbon($rest->rest_finish);
-            $rest_time = $rest_finish->diffInSeconds($rest_start);
-            $sum_rest_time = $sum_rest_time + $rest_time;
-        }
+        $work_time = $this->diffTime($this->work_start, $this->work_finish);
+        $sum_rest_time = $this->getSumRestTime();
         $sum_work_time = $work_time - $sum_rest_time;
-        $work_time_hour = sprintf('%02d', floor($sum_work_time / 3600));
-        $work_time_minute = sprintf('%02d', floor(($sum_work_time / 60) % 60));
-        $work_time_second = sprintf('%02d', $sum_work_time % 60);
-        return "${work_time_hour}:${work_time_minute}:${work_time_second}";
+        return $this->formatTime($sum_work_time);
     }
 }
